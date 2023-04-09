af972a85fac1f78299f207f0dac4fa2af449b976
Fantom <126494915+fantomcvc@users.noreply.github.com>
2023-04-09 18:48:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from setuptools import setup
 
 setup(
 	name="aiohelpersms",
-	version='0.1.0',
+	version='0.1.1',
 	url="https://github.com/fantomcvc/aiohelpersms",
 	license="MIT License",
 	author="fantomcvc",
-	description="aiohelpersms",
-	install_requires=[
-		'aiohttp',
-		'pydantic',
-		'typing',
-	],
+	author_email='fantomcvv@gmail.com',
+	description="Async API wrapper for helpersms ",
+	packages=['aiohelpersms'],
 )
